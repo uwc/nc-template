514786599510c6286471f8398bbb69a71ce6a05e
Connor Bär <hello@connorbaer.io>
2016-09-16 15:11:35 +0200

    Implements headroom.js, enhancements to navigation.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,40 @@
 
 $( document ).on( 'turbolinks:load', function() {
 
+	// RESPONSIVE NAV //
+
 	var nav = responsiveNav( '.nav-collapse', {
 		customToggle: 'js-menu', /* Selector: Specify the ID of a custom toggle. */
 		closeOnNavClick: true, /* Boolean: Close the navigation when one of the links are clicked. */
 		openPos: 'relative' /* String: Position of the opened nav, relative or static. */
 	});
 
-	// Initialize and configure SmoothScroll.
+	// SMOOTHSCROLL //
+
 	smoothScroll.init( { offset: 36 } );
+
+	// HEADROOM //
+
+	// Grab an element.
+	var hrBody = document.body;
+
+	// Construct an instance of Headroom, passing the element.
+	var headroom  = new Headroom( hrBody, {
+
+	    // Vertical offset in px before element is first unpinned.
+	    offset: 64,
+
+	    // Scroll tolerance in px before state changes for up/down scroll.
+	    tolerance: {
+	        up: 10,
+	        down: 5
+	    }
+	});
+
+	// Initialise.
+	headroom.init();
+
+	// SLICK LIGHTBOX //
 
 	$( '.gallery' ).each( function() {
         $( this ).slickLightbox( {
@@ -24,22 +50,26 @@ $( document ).on( 'turbolinks:load', function() {
 		});
 	});
 
+	// CONTACT BUTTON //
+
 	$( '#js-contact' ).on( 'click', function() {
 		$( '#js-body' ).toggleClass( '-contact-open' );
 	});
 
+	// SEARCH //
+
 	$( '#js-search' ).on( 'click', function() {
 		$( '#js-body' ).toggleClass( '-search-open' );
-		$( '.site-search' ).find( 'input' ).focus();
+		// $( '.site-search' ).find( 'input' ).focus();
 	});
 
-	// Shows the gradient top border of the navigation bar to indicate that the page has loaded.
-	$( '#js-navigation' ).addClass( '-loaded' );
+	// Shows a gradient bar at the top of the navigation to indicate that the page has loaded.
+	$( '#js-body' ).addClass( '-turbolinks-loaded' );
 });
 
-// Hides the gradient top border of the navigation bar to allow the progress bar to show.
+// Hides the gradient bar at the top of the navigation to allow the progress bar to show.
 $( document ).on( 'turbolinks:click', function() {
-	$( '#js-navigation' ).removeClass( '-loaded' );
+	$( '#js-body' ).removeClass( '-turbolinks-loaded' );
 });
 
 // Open and close the search in the navigation bar.
