b1c387c8479c31651c27ec5053228fddebfca2f3
Connor Bär <hello@connorbaer.io>
2016-09-17 16:58:07 +0200

    Bumps version to 1.4.0.

--- src/components/site-contact.php
+++ src/components/site-contact.php
@@ -25,7 +25,7 @@ if ( $name && $contact && $email ) :
 			<button id="js-contact" class="contact-button"><span class="screen-reader-text"><?php esc_attr_e( 'Contact us', 'uwc-website' ) ?></span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="contact-open" d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"/><path class="contact-close" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg></button>
 			<div class="contact-box">
 				<div class="contact-header">
-				<?php if( get_field( 'profile_picture', $id ) ) :
+				<?php if ( get_field( 'profile_picture', $id ) ) :
 						$image = get_field( 'profile_picture', $id );
 						$size = 'thumbnail';
 						$url = $image['sizes'][ $size ];
